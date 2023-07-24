95189398e02e1d6d1441b21968d00a9f8d17f661
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-07-24 11:51:03 +0600

    userSchema added

---
+++ models/user.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+
+    role: {
+      type: String,
+      enum: ["user", "seller", "admin"],
+      default: "user",
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
