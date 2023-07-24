fa4b780dad13903c9f98b98b79510a9372ee95b3
Sumaiya Rimu <sumaiyarimu22@gmail.com>
2023-07-24 14:44:48 +0600

    login controller added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,4 @@
+const { createToken } = require("../helpers/token.helper");
 const User = require("../models/user.model");
 
 const createUser = async (req, res) => {
@@ -13,12 +14,26 @@ const createUser = async (req, res) => {
       occupation
     );
 
-    res.status(200).json(user);
+    const token = createToken(user._id);
+
+    res.status(200).json({ user, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
-const loginUser = async (req, res) => {};
+
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.login(email, password);
+
+    const token = createToken(user._id);
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
 module.exports = {
   createUser,
